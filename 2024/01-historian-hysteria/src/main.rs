606e0030442eeb161b350a323de5460e3be0a5db
Matthew Ibbetson <mmibbetson@gmail.com>
2024-12-01 11:11:38 +0200

    solution: add solution for day 1 of aoc2024

---
+++ 2024/01-historian-hysteria/src/main.rs
@@ -0,0 +1,58 @@
+use std::{env, fs};
+
+fn main() {
+    let args = env::args().collect::<Vec<String>>();
+    let input_file = &args[1];
+
+    println!("Finding solution for: {input_file}");
+
+    let input = fs::read_to_string(&args[1]).expect("Failed to read input file.");
+    let (left, right) = separate_id_lists(&input);
+    let diff_sum = id_list_difference(&left, &right);
+    let sim_score = similarity_score(&left, &right);
+
+    println!("The similarity score for these lists is:\n\n    {sim_score}\n");
+    println!("The total sum of the differences of the location ID's is:\n\n    {diff_sum}\n");
+}
+
+fn separate_id_lists(input: &str) -> (Vec<i32>, Vec<i32>) {
+    let (mut left, mut right) = input
+        .lines()
+        .map(|l| {
+            let mut iter = l
+                .split_whitespace()
+                .map(|c| c.parse::<i32>().expect("Failed to parse location ID!"));
+
+            (
+                iter.next().expect("There was no first ID on a line."),
+                iter.next().expect("There was no second ID on a line."),
+            )
+        })
+        .fold(
+            (Vec::new(), Vec::new()),
+            |(mut acc_l, mut acc_r), (cur_l, cur_r)| {
+                acc_l.push(cur_l);
+                acc_r.push(cur_r);
+
+                (acc_l, acc_r)
+            },
+        );
+
+    left.sort();
+    right.sort();
+
+    (left, right)
+}
+
+fn id_list_difference(left: &Vec<i32>, right: &Vec<i32>) -> i32 {
+    left.iter()
+        .zip(right.iter())
+        .map(|(l, r)| if l > r { l - r } else { r - l })
+        .sum()
+}
+
+fn similarity_score(left: &Vec<i32>, right: &Vec<i32>) -> i32 {
+    left.iter()
+        .map(|l| l * right.iter().filter(|r| *r == l).count() as i32)
+        .sum()
+}
